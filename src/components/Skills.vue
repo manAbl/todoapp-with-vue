edf6c39b1136245e3b9e51de23096f2a0b1b448a
manuel blanco <manuel.22.lamas@gmail.com>
2018-06-20 17:31:13 -0400

    created layout, form and data logic creation

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="hello">
-    Skills
+    <div class="holder">
+
+      <form @submit.prevent='addSkill'>
+        <input type="text" placeholder="Enter a skill you have .. " v-model="skill">
+      </form>
+
+      <ul>
+        <li v-for="(data, index) in skills" :key='index'>
+        {{ data.skill }}
+        </li>
+      </ul>
+      <p> These are the skills that you posess</p>
+    </div>
   </div>
 </template>
 
@@ -8,27 +20,68 @@
 
 export default {
   name: 'Skills',
-  props : {
-
+  data () {
+    return {
+      skill: '',
+      skills: [
+        {'skill': 'Vue.js'},
+        {'skill': 'front end developer'},
+      ],
+    }
   },
+  methods: {
+    addSkill() {
+      this.skills.push({ skill: this.skill })
+      this.skill = '';
+      console.log('This checkbox value is: ' + this.checked)
+    }
+  }
 }
+
 
 </script>
 
+<!---------------------------------------------->
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+
+.holder {
+  background: #fff;
 }
+
 ul {
-  list-style-type: none;
+  margin: 0;
   padding: 0;
+  list-style-type: none;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #E0EDF4;
+  border-left: 5px solid #3EB3F6;
+  margin-bottom: 2px;
+  color: #3E5252;
 }
-a {
-  color: #42b983;
+
+p {
+  text-align:center;
+  padding: 30px 0;
+  color: gray;
 }
+
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
+
+input {
+  border: 0;
+  padding: 20px;
+  color: #867f7f;
+  font-size: 1.3em;
+  width: calc(100% - 40px);
+  background-color: #323333;
+}
+
 </style>
